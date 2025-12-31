2f7be1799be22eed4f501706741aab10e82cbcd2
Amirsohail335 <amirsohail335@gmail.com>
2025-12-31 16:15:45 +0530

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./views/auth/Login";
+import Register from "./views/auth/Register";
+import ForgotPassword from "./views/auth/ForgotPassword";
+import Logout from "./views/auth/Logout";
+import CreatePassword from "./views/auth/CreatePassword";
+import MainWrapper from "./layouts/MainWrapper";
+import StoreHeader from "./base/StoreHeader";
+import StoreFooter from "./base/StoreFooter";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <StoreHeader />
+        {/* // Wrap everything in the 'MainWrapper' component. */}
+        <MainWrapper>
+          <Routes>
+            {/* Authentication Routes */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/create-new-password" element={<CreatePassword />} />
+          </Routes>
+        </MainWrapper>
+        <StoreFooter />
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
